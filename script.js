b8e9280c22d14a1c2a1793a5e42c48825e1248a7
Augusto Corvalan <augusto.corvalan@1stdibs.com>
2020-10-23 18:59:15 -0700

    create script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+const Body = u('body')
+
+function getSeedWords() {
+  const SEED_WORDS = [
+    "carne"
+  ]
+  return SEED_WORDS
+}
+
+function createTextElement(text, classes="text-element") {
+    const textElement = u('<p>').addClass(classes).text(text)
+    return textElement
+}
+
+function getInterval() {
+  const INTERVAL = 1000
+  return INTERVAL
+}
+
+function init() {
+  let singleSeedWord = ''
+  let seedWords = []
+  
+  function change() {
+    seedWords = getSeedWords()
+    singleSeedWord = seedWords()[0]
+    
+    draw()
+  }
+  
+  function draw() {
+    const TextElement = createTextElement(singleSeedWord, 'seed-word')  
+    Body.append(TextElement)
+  }
+  
+
+  change()
+}
+
+
+init()
